478e7fa8c04453f58d2e2af823d145d84444392e
Samuel <roths@informatik.uni-freiburg.de>
2018-06-22 19:22:08 +0200

    Refactor frontend search.

--- hanfor/static/js/requirements.js
+++ hanfor/static/js/requirements.js
@@ -8,6 +8,7 @@ require('jquery-ui/ui/effects/effect-highlight');
 require('./bootstrap-tokenfield.js');
 
 // Globals
+const { SearchNode } = require('./datatables-advanced-search.js');
 let Fuse = require('fuse.js');
 let { Textcomplete, Textarea } = require('textcomplete');
 let fuse = new Fuse([], {});
@@ -17,8 +18,7 @@ let available_status = ['', 'Todo', 'Review', 'Done'];
 let available_types = [''];
 let available_vars = [''];
 let visible_columns = [true, true, true, true, true, true];
-let search_tree = undefined;
-let filter_tree = undefined;
+let filter_search_array = [];
 let get_query = JSON.parse(search_query); // search_query is set in index.html
 let tag_colors = {};
 let type_inference_errors = [];
@@ -27,290 +27,20 @@ let filter_status_string = sessionStorage.getItem('filter_status_string');
 let filter_tag_string = sessionStorage.getItem('filter_tag_string');
 let filter_type_string = sessionStorage.getItem('filter_type_string');
 
-// Search query grammar declarations.
-const operators = {":AND:": 1, ":OR:": 1};
-const leftAssoc = {":AND:": 1, ":OR:": 1};
-const rightAssoc = {};
-const parantheses = {"(": 1, ")": 1};
-const precedenceOf = {":AND:": 3, ":OR:": 2};
-
-/**
- * SearchNode represents one node in a search expression used to filter the requirements table.
- */
-class SearchNode {
-    constructor(value) {
-        this.left = false;
-        this.value = value;
-        this.right = false;
-        this.col_target = -1;
-        this.update_target();
-    }
-
-    update_target() {
-        const col_string_index = this.value.indexOf(':COL_INDEX_');
-        if (col_string_index >= 0) {
-            const target_index = parseInt(this.value.substring(col_string_index + 11, col_string_index + 13));
-            if (target_index >= 0) {
-                this.value = this.value.substring(col_string_index + 14);
-                this.col_target = target_index;
-            }
-        }
-    }
-
-    static is_search_string(token) {
-        return !(token in parantheses || token in operators);
-    }
-
-    static to_string(tree) {
-        let repr = '';
-        if (tree.left !== false) {
-            repr += SearchNode.to_string(tree.left) + ' ';
-        }
-        repr += tree.value;
-        if (tree.right !== false) {
-            repr += ' ' + SearchNode.to_string(tree.right);
-        }
-        return repr;
-    }
-
-    static peek(array) {
-        return array[array.length - 1];
-    }
-
-    /**
-     * Parses a search array to a binary search tree using shunting yard algorithm.
-     * @param array
-     * @returns {*}
-     */
-    static searchArrayToTree(array) {
-
-        let output_tree_stack = [], op_stack = [];
-
-        for (let i = 0, length = array.length; i < length;  i++) {
-            const token = array[i]; // current token
-
-            // If token is a search string, add it to the output_tree_stack as a singleton tree.
-            if (SearchNode.is_search_string(token))
-                output_tree_stack.push(new SearchNode(token));
-
-            else if (token in operators) {
-                // We encountered an operator.
-                while (op_stack.length) {
-                    // As long as there is an operator (prev_op) at the top of the op_stack
-                    const prev_op = SearchNode.peek(op_stack);
-                    if (prev_op in operators && (
-                            // and token is left associative and precedence <= to that of prev_op,
-                            (token in leftAssoc &&
-                                (precedenceOf[token] <= precedenceOf[prev_op])) ||
-                            // or token is right associative and its precedence < to that of prev_op,
-                            (token in rightAssoc &&
-                                (precedenceOf[token] < precedenceOf[prev_op]))
-                        )) {
-                        // Pop last two subtrees and make them children of a new subtree (with prev_op as root).
-                        let right = output_tree_stack.pop(), left = output_tree_stack.pop();
-                        let sub_tree = new SearchNode(op_stack.pop());
-                        sub_tree.left = left;
-                        sub_tree.right = right;
-                        output_tree_stack.push(sub_tree);
-                    } else {
-                        break;
-                    }
-                }
-                op_stack.push(token);
-            }
-
-            // If token is opening parenthesis, just push to the op_stack.
-            else if (token === "(")
-                op_stack.push(token);
-
-            // If token is closing parenthesis:
-            else if (token === ")") {
-
-                let has_opening_match = false;
-
-                // Search for opening paranthesis in op_stack.
-                while (op_stack.length) {
-                    const op = op_stack.pop();
-                    if (op === "(") {
-                        has_opening_match = true;
-                        break;
-                    } else {
-                        // Until match pop operators off the op_stack and create a new subtree with operator as root.
-                        let right = output_tree_stack.pop();
-                        let left = output_tree_stack.pop();
-                        let sub_tree = new SearchNode(op);
-                        sub_tree.left = left;
-                        sub_tree.right = right;
-                        output_tree_stack.push(sub_tree);
-                    }
-                }
-                if (!has_opening_match)
-                    throw "Error: parentheses mismatch.";
-            }
-            else throw "Error: Token unknown: " + token;
-        }
-
-        // No more tokens in input but operator tokens in the op_stack:
-        while (op_stack.length) {
-
-            const op = op_stack.pop();
-
-            if (op === "(" || op === ")")
-                throw "Error: Parentheses mismatch.";
-
-            // Create new subtree with op as root.
-            let right = output_tree_stack.pop();
-            let left = output_tree_stack.pop();
-            let sub_tree = new SearchNode(op);
-            sub_tree.left = left;
-            sub_tree.right = right;
-            output_tree_stack.push(sub_tree);
-        }
-
-        // The last remaining node should be the root of our complete search tree.
-        return output_tree_stack[0];
-    }
-
-    /**
-     * Splits a search query into array where each element is one token.
-     * @param query
-     * @param target_col optional target col to restrict the search on a specific col.
-     * @returns {*|string[]}
-     */
-    static awesomeQuerySplitt0r(query, target_col=undefined) {
-        // Split by :AND:
-        let result = query.split(/(:OR:|:AND:|\(|\))/g);
-        result = result.filter(String); // Remove empty elements.
-        // If the resulting tree should be restricted to a col..
-        if (target_col !== undefined) {
-            for (let i = 0, length = result.length; i < length;  i++) {
-                // Add :COL_INDEX_<target_col>: to each search string (not a operator or parenthesis).
-                if (!(result[i] in operators || result[i] in parantheses)) {
-                    result[i] = ':COL_INDEX_' + ("00" + target_col).slice(-2) + ':' + result[i];
-                }
-            }
-        }
-        return result;
-    }
-
-    /**
-     * Create a Search Tree from search query.
-     * @param query
-     * @param target_col optional target col to restrict the search on a specific col.
-     * @returns {*}
-     */
-    static fromQuery(query, target_col=undefined) {
-        return SearchNode.searchArrayToTree(SearchNode.awesomeQuerySplitt0r(query, target_col));
-    }
-
-}
-
-/**
- * Check is value is in string. Support `"` for exact and `""` padding for exclusive match.
- * @param value string to be converted to regex and matched agains string.
- * @param string
- * @returns {boolean}
- */
-function check_value_in_string(value, string) {
-    // We support value to be `
-    //  * "<inner>"` for exact match.
-    //  * ""<inner>"" for exclusive match.
-
-    if (value.startsWith('""') && value.endsWith('""')) {
-        value = '^\\s*' + value.substr(2, (value.length - 4)) + '\\s*$';
-    } else {
-        // replace " by \b to allow for exact matches.
-        // In the input we escaped " by \" so we would like to apply (?<!\\)\"
-        // since javascript does not allow negative look behinds we do
-        // something like ([^\\])(\") and replace the 2. group by \b but keeping \" intact.
-        value = value.replace(/([^\\])?\"/g, "$1\\b");
-    }
-
-    const regex = new RegExp(value, "i");
-    return regex.test(string);
-
-}
-
-/**
- * Apply a search expression tree on row data.
- * @param tree
- * @param data
- * @returns {bool}
- */
-function evaluateSearchExpressionTree(tree, data) {
-    // Root node
-    if (tree === undefined) {
-        return true;
-    }
-
-    // Leaf node.
-    if (tree.left === false && tree.right === false) {
-        // First build the string to search.
-        let string = '';
-        // We have a specific target.
-        if (tree.col_target !== -1) {
-            string = data[tree.col_target];
-        } else {
-            // We search in all visible columns.
-            for (let i = 0; i < visible_columns.length; i++) {
-                if (visible_columns[i]) {
-                    string += data[i] + ' ';
-                }
-            }
-        }
-        const not_index = tree.value.indexOf(':NOT:');
-        if (not_index >= 0) { // Invert search on :NOT: keyword.
-            return !check_value_in_string(tree.value.substring(not_index + 5), string);
-        } else {
-            return check_value_in_string(tree.value, string);
-        }
-    }
-
-    // evaluate left tree
-    let left_sub = evaluateSearchExpressionTree(tree.left, data);
-
-    // evaluate right tree
-    let right_sub = evaluateSearchExpressionTree(tree.right, data);
-
-    // Apply operations
-    if (tree.value === ':AND:') {
-        return (left_sub && right_sub);
-    }
-
-    if (tree.value === ':OR:') {
-        return (left_sub || right_sub);
-    }
-}
-
-
-/**
- * Hanfor specific requirements table filtering.
- * In user search queries we use:
- * -> :OR: and :AND: to concatenate queries.
- * -> " to indicate the beginning or end of a search sequence.
- */
-$.fn.dataTable.ext.search.push(
-    function( settings, data, dataIndex ) {
-        // data contains the row. data[0] is the content of the first column in the actual row.
-        // Return true to include the row into the data. false to exclude.
-        return evaluateSearchExpressionTree(search_tree, data) && evaluateSearchExpressionTree(filter_tree, data);
-    }
-);
 
 /**
  * Update the search expression tree.
  */
-function update_search_tree() {
+function update_search() {
     req_search_string = $('#search_bar').val().trim();
-    search_tree = SearchNode.fromQuery(req_search_string);
     sessionStorage.setItem('req_search_string', req_search_string);
 }
 
 /**
  * Update the filter search tree used to filter the table by the values from the Filter tab.
  */
-function update_filter_tree() {
-    let search_array = [];
+function update_filter() {
+    filter_search_array = [];
     function pad_with_parantheses(array) {
         return ["("].concat(array, [")"]);
     }
@@ -327,15 +57,13 @@ function update_filter_tree() {
     filter_tag_string = $('#tag-filter-input').val();
     filter_type_string = $('#type-filter-input').val();
 
-    search_array = add_query(search_array, filter_type_string, 4);
-    search_array = add_query(search_array, filter_tag_string, 5);
-    search_array = add_query(search_array, filter_status_string, 6);
-
     sessionStorage.setItem('filter_status_string', filter_status_string);
     sessionStorage.setItem('filter_tag_string', filter_tag_string);
     sessionStorage.setItem('filter_type_string', filter_type_string);
 
-    filter_tree = SearchNode.searchArrayToTree(search_array);
+    filter_search_array = add_query(filter_search_array, filter_type_string, 4);
+    filter_search_array = add_query(filter_search_array, filter_tag_string, 5);
+    filter_search_array = add_query(filter_search_array, filter_status_string, 6);
 }
 
 
@@ -951,7 +679,7 @@ function init_datatable_manipulators(requirements_table) {
     // Bind big custom searchbar to search the table.
     $('#search_bar').keypress(function(e) {
         if(e.which === 13) { // Search on enter.
-            update_search_tree();
+            update_search();
             requirements_table.draw();
         }
     });
@@ -979,7 +707,7 @@ function init_datatable_manipulators(requirements_table) {
         .on('focus', function() { $(this).keydown(); })
         .on('keypress', function (e) {
             if (e.which === 13) { // Search on Enter.
-                update_filter_tree();
+                update_filter();
                 requirements_table.draw();
             }
         });
@@ -995,11 +723,9 @@ function init_datatable_manipulators(requirements_table) {
         $('#tag-filter-input').val('').effect("highlight", {color: 'green'}, 500);
         $('#type-filter-input').val('').effect("highlight", {color: 'green'}, 500);
         $('#search_bar').val('').effect("highlight", {color: 'green'}, 500);
-        search_tree = undefined;
-        filter_tree = undefined;
-        requirements_table.search( '' ).columns().search( '' ).draw();
-        update_filter_tree();
-        update_search_tree();
+        update_filter();
+        update_search();
+        requirements_table.draw();
     });
 
     // Listen for tool section triggers.
@@ -1145,8 +871,19 @@ function init_datatable(columnDefs) {
             init_datatable_manipulators(requirements_table);
             // Process URL search query.
             process_url_query(requirements_table, get_query);
-            update_search_tree();
-            update_filter_tree();
+            update_search();
+            update_filter();
+
+            // Enable Hanfor specific requirements table filtering.
+            $.fn.dataTable.ext.search.push(
+                function( settings, data, dataIndex ) {
+                    // data contains the row. data[0] is the content of the first column in the actual row.
+                    // Return true to include the row into the data. false to exclude.
+                    return SearchNode.fromQuery(query=req_search_string).evaluate(data, visible_columns) &&
+                        SearchNode.searchArrayToTree(filter_search_array).evaluate(data, visible_columns);
+                }
+            );
+
             this.api().draw();
         }
     });
