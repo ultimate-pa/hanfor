64f2bb0939a2bd466a90e0db7abb8b5a6d503819
danieldietsch <daniel.dietsch@gmail.com>
2018-07-02 09:51:21 +0200

    some cleanup, another variant of signal routing

--- hanfor/guesser/RegexGuesser.py
+++ hanfor/guesser/RegexGuesser.py
@@ -13,7 +13,8 @@ class RegexGuesser(AbstractGuesser):
     def guess(self):
         matcher = [
             self.__guess_assign,
-            self.__guess_signal_routing
+            self.__guess_signal_routing,
+            self.__guess_signal_routing_value
         ]
 
         # If one matches, use it
@@ -32,37 +33,31 @@ class RegexGuesser(AbstractGuesser):
 
     def __guess_assign(self):
         # try `var_a := var_b`
-        regex = r"(.*):=(.*)"
-
-        result = re.finditer(regex, self.requirement.description, re.MULTILINE)
-        matches = list(result)
+        matches = self.__match(r"(.*):=(.*)")
         if len(matches) != 1:
             return None, None
 
-        match = matches[0]
-        scoped_pattern = ScopedPattern(
-            Scope['GLOBALLY'],
-            Pattern(name='Invariant')
-        )
-        mapping = {
-            'R': match.group(1).strip(),
-            'S': match.group(2).strip()
-        }
-        return scoped_pattern, mapping
+        return self.__create_invariant(matches[0])
 
     def __guess_signal_routing(self):
-        regex = r"^The signal (\w+) shall be routet on \w+ \((\w+)\)[\s|\.]*$"
-        result = re.finditer(regex, self.requirement.description, re.MULTILINE)
-        matches = list(result)
+        matches = self.__match(r"^The signal (\w+) shall be routet on \w+ \((\w+)\)[\s|\.]*$")
         if len(matches) != 1:
             return None, None
 
+        return self.__create_bounded_response(matches[0])
+
+    def __guess_signal_routing_value(self):
+        matches = self.__match(r"^The value of the parameter (\w+) shall be routet on \w+ \((\w+)\)[\s|\.]*$")
+        if len(matches) != 1:
+            return None, None
+
+        return self.__create_bounded_response(matches[0])
+
+    def __create_bounded_response(self, match):
         """
-        Becomes 'BoundedResponse':
+        Creates 'BoundedResponse' pattern with MAX_TIME as T
         'it is always the case that if {R} holds, then {S} holds after at most {T} time units',
         """
-
-        match = matches[0]
         scoped_pattern = ScopedPattern(
             Scope['GLOBALLY'],
             Pattern(name='BoundedResponse')
@@ -73,3 +68,18 @@ class RegexGuesser(AbstractGuesser):
             'T': 'MAX_TIME'
         }
         return scoped_pattern, mapping
+
+    def __create_invariant(self, match):
+        scoped_pattern = ScopedPattern(
+            Scope['GLOBALLY'],
+            Pattern(name='Invariant')
+        )
+        mapping = {
+            'R': match.group(1).strip(),
+            'S': match.group(2).strip()
+        }
+        return scoped_pattern, mapping
+
+    def __match(self, regex):
+        result = re.finditer(regex, self.requirement.description, re.MULTILINE)
+        return list(result)
