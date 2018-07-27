1857404dae0085c4e45d4784d19a676aebf5acdc
bree@titan <loeffler.uni@gmail.com>
2018-07-27 15:20:58 +0200

    Updated Regex Guesser: experimental if-then guessing
    regex guesser can now guess "IF {R} THEN {S}" to some degree. The implementation is quick and dirty,  unoptimized and does not support all operators.

--- hanfor/guesser/RegexGuesser.py
+++ hanfor/guesser/RegexGuesser.py
@@ -1,9 +1,11 @@
 import re
+from functools import reduce
 
 from guesser.AbstractGuesser import AbstractGuesser
 from guesser.Guess import Guess
 from guesser.guesser_registerer import register_guesser
 from reqtransformer import ScopedPattern, Scope, Pattern
+
 
 
 @register_guesser
@@ -12,6 +14,7 @@ class RegexGuesser(AbstractGuesser):
 
     def guess(self):
         matcher = [
+            self.__guess_if_then,
             self.__guess_assign,
             self.__guess_signal_routing,
             self.__guess_signal_routing_message,
@@ -20,17 +23,19 @@ class RegexGuesser(AbstractGuesser):
 
         # If one matches, use it
         for match in matcher:
-            scoped_pattern, mapping = match()
-            if not scoped_pattern:
-                continue
-            self.guesses.append(
-                Guess(
-                    score=1,
-                    scoped_pattern=scoped_pattern,
-                    mapping=mapping
+            try:
+                scoped_pattern, mapping = match()
+                if not scoped_pattern:
+                    continue
+                self.guesses.append(
+                    Guess(
+                        score=1,
+                        scoped_pattern=scoped_pattern,
+                        mapping=mapping
+                    )
                 )
-            )
-            break
+            except:
+                pass
 
     def __guess_assign(self):
         # try `var_a := var_b`
@@ -61,6 +66,115 @@ class RegexGuesser(AbstractGuesser):
 
         return self.__create_bounded_response(matches[0])
 
+    def __guess_if_then(self):
+        #if "== ->" in self.requirement.description or "==->" in self.requirement.description:
+        #    return None, None
+
+        # match IF ... THEN ... case.
+        match = re.search(r'IF(.*)THEN(.*)(Hint: .*)*', self.requirement.description, re.DOTALL)
+
+        if not match:
+            return None, None
+
+        # There is IF (AND-logic) .. THEN and IF (OR-logic) .. THEN
+        and_logic = True
+        if "(or-logic)" in match.group(1).lower():
+            and_logic = False
+
+        var1 = self.__process_if_part(match.group(1).replace("(AND-logic)", "").replace("(OR-logic)", ""), and_logic)
+        var2 = self.__process_if_part(match.group(2), and_logic)
+
+        if var1 is None or var2 is None:
+            return None, None
+
+        return self.__create_bounded_response_if_then(var1=var1, var2=var2)
+
+    def __process_if_part(self, part, and_logic):
+        terms = part.strip().split("\n")
+        processed = []
+        for term in terms:
+            sub_terms = re.split("OR|AND", term)
+            new_sub_terms = []
+            for sub_term in sub_terms:
+                new_sub_term = self.__process_term(sub_term.strip())
+                if new_sub_term is not None:
+                    new_sub_terms.append(new_sub_term)
+            if new_sub_terms:
+                if "OR" in term:
+                    new_term = " || ".join(new_sub_terms)
+                elif "AND" in term:
+                    new_term = " && ".join(new_sub_terms)
+                else:
+                    new_term = "".join(new_sub_terms)
+                processed.append("(%s)" % new_term)
+        if and_logic:
+            new_part = "\n&&".join(processed)
+        else:
+            new_part = "\n||".join(processed)
+        return new_part
+
+    def __process_term(self, term):
+        equals = re.search(r'(.*)(==)(.*)', term.replace(" := ", " == ").replace("== ->", "==").replace("==->", "=="), re.DOTALL)
+        not_equals = re.search(r'(.*)(!=)(.*)', term, re.DOTALL)
+        gt_lt = re.search(r'(.*)\s(<|>=|>|<=)\s(.*)', term, re.DOTALL)
+        not_available = re.search(r'(.*) not available', term, re.DOTALL)
+        available = re.search(r'(.*)(is)* available', term, re.DOTALL)
+        received = re.search(r'(.*)(is)* received', term, re.DOTALL)
+        not_received = re.search(r'(.*) not received', term, re.DOTALL)
+        timer = re.search(r'(start|stop) timer (.*)', term, re.DOTALL)
+
+        sep = "=="
+        case = False
+
+        if equals:
+            sep = "=="
+            case = equals
+        elif not_equals:
+            sep = "!="
+            case = not_equals
+        elif not_available:
+            sep = None
+            case = not_available
+        elif not_received:
+            sep = None
+            case = not_received
+        elif received:
+            sep = None
+            case = received
+        elif available:
+            sep = None
+            case = available
+        elif gt_lt:
+            sep = gt_lt.group(2)
+            case = gt_lt
+        elif timer:
+            sep = "=="
+            case = timer
+
+        if equals or not_equals:
+            LHS = case.group(1).strip()
+            RHS = case.group(3).strip().replace('"', '').replace(" ", "_").lower().replace("true", "1").replace("false", "0").replace("(", "").replace(")", "").replace("&", "AND")
+            if RHS != "1" and RHS != "0":
+                RHS = "%s_%s" % (LHS, RHS.upper())
+            new_term = "%s %s %s" % (LHS, sep, RHS)
+        elif not_available or not_received:
+            new_term = "!%s" % case.group(1).strip().replace(" is", "")
+        elif received or available:
+            new_term = "%s" % case.group(1).strip().replace(" is", "")
+        elif gt_lt:
+            LHS = case.group(1).strip()
+            RHS = case.group(3).strip().replace('"', '').replace(" ", "_").replace("(", "").replace(")", "").replace("&", "AND")
+            new_term = "%s %s %s" % (LHS, sep, RHS)
+        elif timer:
+            LHS = case.group(2).strip()
+            RHS = 0
+            new_term = "%s %s %s" % (LHS, sep, RHS)
+        else:
+            new_term = None
+
+        return new_term
+
+
     def __create_bounded_response(self, match):
         """
         Creates 'BoundedResponse' pattern with MAX_TIME as T
@@ -90,6 +204,24 @@ class RegexGuesser(AbstractGuesser):
             'R': match.group(1).strip(),
             'S': match.group(2).strip()
         }
+        return scoped_pattern, mapping
+
+    def __create_bounded_response_if_then(self, var1, var2):
+        """
+        Creates 'BoundedResponse' pattern with MAX_TIME as T
+        'it is always the case that if {R} holds, then {S} holds after at most {T} time units',
+        """
+        scoped_pattern = ScopedPattern(
+            Scope['GLOBALLY'],
+            Pattern(name='BoundedResponse')
+        )
+
+        mapping = {
+            'R': "%s" % var1,
+            'S': "%s" % var2,
+            'T': 'MAX_TIME'
+        }
+
         return scoped_pattern, mapping
 
     def __match(self, regex):
