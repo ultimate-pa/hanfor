977193e40f2f70de69535a2baebc6074290506cd
Samuel <roths@informatik.uni-freiburg.de>
2018-11-27 01:35:24 +0100

    Implement reports as a new ressource. Ref #107

---
+++ hanfor/ressources/ressource.py
@@ -0,0 +1,69 @@
+from flask import jsonify
+from utils import MetaSettings
+
+
+class Ressource:
+    def __init__(self, app, request):
+        self.app = app
+        self.request = request
+        self.response = Response()
+        self._meta_settings = MetaSettings(self.app.config['META_SETTTINGS_PATH'])
+
+    @property
+    def meta_settings(self):
+        return self._meta_settings
+
+    def apply_request(self):
+        getattr(self, self.request.method)()
+        return jsonify(self.response), self.response.http_status
+
+    def GET(self):
+        raise NotImplementedError
+
+    def POST(self):
+        raise NotImplementedError
+
+    def DELETE(self):
+        raise NotImplementedError
+
+
+class Response(dict):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self['success'] = True
+        self['errormsg'] = ''
+        self['HTTP_status'] = 200
+
+    @property
+    def success(self):
+        return self['success']
+
+    @success.setter
+    def success(self, val):
+        if val == False:
+            self['errormsg'] = 'Sorry, could not parse your request.'
+        self['success'] = val
+
+    @property
+    def http_status(self):
+        return self['HTTP_status']
+
+    @http_status.setter
+    def http_status(self, code):
+        self['HTTP_status'] = code
+
+    @property
+    def errormsg(self):
+        return self['errormsg']
+
+    @errormsg.setter
+    def errormsg(self, msg):
+        self['errormsg'] = msg
+
+    @property
+    def data(self):
+        return self['data']
+
+    @data.setter
+    def data(self, data):
+        self['data'] = data
