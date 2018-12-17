1b3f38199db61ae17d6bdae45df4601602101f35
Samuel <roths@informatik.uni-freiburg.de>
2018-12-17 16:00:30 +0100

    Add hanfor init system test.

--- hanfor/tests/test_init.py
+++ hanfor/tests/test_init.py
@@ -1,8 +1,14 @@
 from app import app, api, set_session_config_vars, create_revision
 import os
+import shutil
 import utils
 from unittest import TestCase
 from unittest.mock import patch
+
+HERE = os.path.dirname(os.path.realpath(__file__))
+TESTS_BASE_FOLDER = os.path.join(HERE, 'test_sessions')
+TEST_CSV = os.path.join(HERE, 'test_sessions/test_init/simple.csv')
+TEST_TAG = 'simple'
 
 
 def mock_user_input(*args, **kwargs) -> str:
@@ -16,6 +22,10 @@ def mock_user_input(*args, **kwargs) -> str:
     except:
         count = 0
 
+    if count == len(mock_results):
+        # Restart after reached the end.
+        count = 0
+
     result = mock_results[count]
     print('Mocked input: {}'.format(result))
     return str(result)
@@ -23,15 +33,81 @@ def mock_user_input(*args, **kwargs) -> str:
 
 class TestInit(TestCase):
     def setUp(self):
-        self.args = utils.HanforArgumentParser(app).parse_args(['test_sessions/test_init/simple.csv', 'simple'])
-        app.config['SESSION_BASE_FOLDER'] = 'test_sessions'
+        self.args = utils.HanforArgumentParser(app).parse_args([TEST_CSV, TEST_TAG])
+        app.config['SESSION_BASE_FOLDER'] = TESTS_BASE_FOLDER
         utils.register_assets(app)
         set_session_config_vars(self.args)
         self.app = app.test_client()
 
     @patch('builtins.input', mock_user_input)
-    def test_init_from_csv(self):
+    def create_revision(self):
         global mock_results
-        mock_results = [0, 2, 3, 1]
+        mock_results = [2, 0, 1, 3]
 
         create_revision(self.args, None)
+
+    def test_1_init_from_csv(self):
+        self.create_revision()
+        self.assertTrue(
+            os.path.isdir(os.path.join(TESTS_BASE_FOLDER, TEST_TAG)),
+            msg='No session folder created.'
+        )
+        self.assertTrue(
+            os.path.isdir(os.path.join(TESTS_BASE_FOLDER, TEST_TAG, 'revision_0')),
+            msg='No revision folder created.'
+        )
+
+        # Check contents.
+        for file in ['SysRS FooXY_42.pickle', 'SysRS FooXY_91.pickle', 'session_status.pickle']:
+            self.assertTrue(
+                os.path.exists(os.path.join(TESTS_BASE_FOLDER, TEST_TAG, 'revision_0', file)),
+                msg='Missing file: {}'.format(file)
+            )
+
+    def test_2_get_requirements(self):
+        self.create_revision()
+        result = self.app.get('api/req/gets')
+        self.maxDiff = None
+        desired_reqs = [
+            {'formal': [],
+             'pattern': 'None',
+             'status': 'Todo',
+             'pos': 1,
+             'vars': {},
+             'scope': 'None',
+             'tags': [],
+             'desc': 'Dont worry, be happy',
+             'csv_data': {
+                 'formal_header': 'Globally, it is never the case, that WORRY holds; Globally, it is always the case, that HAPPY holds.',
+                 'id_header': 'SysRS FooXY_42',
+                 'type_header': 'req',
+                 'desc_header': 'Dont worry, be happy'
+             },
+             'id': 'SysRS FooXY_42',
+             'type_inference_errors': {},
+             'type': 'req'
+             },
+            {'formal': [],
+             'status': 'Todo',
+             'pattern': 'None',
+             'pos': 0,
+             'vars': {},
+             'scope': 'None',
+             'tags': [],
+             'desc': 'always look on the bright side of life',
+             'csv_data': {
+                 'formal_header': 'Globally, it is always the case that POINT_OF_VIEW==BRIGHT_SIDE_OF_LIVE',
+                  'id_header': 'SysRS FooXY_91',
+                 'type_header': 'req',
+                 'desc_header': 'always look on the bright side of life'
+             },
+             'id': 'SysRS FooXY_91',
+             'type_inference_errors': {},
+             'type': 'req'
+             }
+        ]
+        self.assertListEqual(result.json['data'], desired_reqs)
+
+    def tearDown(self):
+        # Clean test dir.
+        shutil.rmtree(os.path.join(TESTS_BASE_FOLDER, TEST_TAG))
