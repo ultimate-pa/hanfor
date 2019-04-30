2d2d635bc7ace83331584010b6244f648a1685e7
Daniel Dietsch <daniel.dietsch@gmail.com>
2019-04-30 20:19:03 +0200

    fixed some things in jenkins.sh

--- hanfor/jenkins.sh
+++ hanfor/jenkins.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-if [ -d "venv" ]; then rm -r venv ; fi 
-mkdir venv 
-cd venv 
-virtualenv -p python3.6 . 
-source ./bin/activate 
-cd .. 
-pip install -r requirements.txt 
-if [ -f "config.py" ] ; rm config.py ; fi 
-mv config.dist.py config.py 
-python run_tests.py 
+if [ -d "venv" ]; then rm -r venv ; fi
+mkdir venv
+cd venv || exit 1
+virtualenv -p python3.6 .
+source ./bin/activate
+cd ..
+pip install -r requirements.txt
+if [ -f "config.py" ] ; then rm config.py ; fi
+cp config.dist.py config.py
+python run_tests.py
