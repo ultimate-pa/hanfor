ff97b8066e7aadfcbf5392a689c066f6719c47cc
Samuel <roths@informatik.uni-freiburg.de>
2019-08-19 14:10:32 +0200

    Add tests for API query.

---
+++ hanfor/tests/test_query_api.py
@@ -0,0 +1,53 @@
+import json
+
+from tests.mock_hanfor import MockHanfor
+from unittest import TestCase
+
+
+class TestEnums(TestCase):
+    def setUp(self) -> None:
+        self.mock_hanfor = MockHanfor(
+            session_tags=['simple'],
+            test_session_source='test_query_api'
+        )
+        self.mock_hanfor.setUp()
+
+    def tearDown(self) -> None:
+        self.mock_hanfor.tearDown()
+
+    def test_new_query(self):
+        self.mock_hanfor.startup_hanfor('simple.csv', 'simple', [])
+        # We expect there is no stored query.
+        initial_query = self.mock_hanfor.app.get('api/query').json['data']
+        self.assertEqual(initial_query, dict())
+
+        # Create a query
+        simple_query = self.mock_hanfor.app.post(
+            '/api/query',
+            json={'name': 'MyQuery', 'query': ':DATA_TARGET:`Tags`has_formalization'}
+        ).json['data']
+        self.assertEqual(
+            simple_query,
+            {
+                'query': ':DATA_TARGET:`Tags`has_formalization',
+                'hits': 1,
+                'result': ['SysRS FooXY_42'],
+                'name': 'MyQuery'
+            }
+        )
+
+        # Create another query
+        simple_query = self.mock_hanfor.app.post(
+            '/api/query',
+            json={'name': 'MyQuery_no_2', 'query': ''}
+        ).json['data']
+        self.assertEqual(
+            simple_query,
+            {
+                'query': '',
+                'hits': 2,
+                'result': ['SysRS FooXY_42', 'SysRS FooXY_91'],
+                'name': 'MyQuery_no_2'
+            }
+        )
+        print(simple_query)
