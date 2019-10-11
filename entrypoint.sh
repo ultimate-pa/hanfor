0d8e9fcae3810c7ac76877a0b550653d52f83702
bree@titan <loeffler.uni@gmail.com>
2019-10-11 13:26:23 +0200

    change entrypoint to use example_input

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 # Start hanfor
 echo "Starting server"
-python3 app.py -c example/example_input.csv --headers='{ "csv_id_header": "ID", "csv_desc_header": "Description", "csv_formal_header": "Hanfor_Formalization", "csv_type_header" : "Type"}' awesome_tag
+python3 app.py -c example_input/example_input.csv --headers='{ "csv_id_header": "ID", "csv_desc_header": "Description", "csv_formal_header": "Hanfor_Formalization", "csv_type_header" : "Type"}' awesome_tag
