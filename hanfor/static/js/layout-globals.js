d5800e4d0772590f41843699bc7069eff7e4dc75
Samuel <roths@informatik.uni-freiburg.de>
2020-09-21 12:00:31 +0200

    Add a dark-mode switch.

--- hanfor/static/js/layout-globals.js
+++ hanfor/static/js/layout-globals.js
@@ -6,3 +6,26 @@ require('jquery-ui/themes/base/all.css');
 require('../css/bootstrap-tokenfield.css');
 require('../css/tether.css');
 require('../css/app.css');
+
+// Dark theme switch.
+function update_color_theme() {
+  const theme_is_dark =
+    localStorage.getItem('color_mode') !== null &&
+    localStorage.getItem('color_mode') === 'dark';
+  theme_is_dark ? document.body.setAttribute('data-theme', 'dark') :
+    document.body.removeAttribute('data-theme');
+}
+
+$('#dark_theme_switch').on('change', function() {
+  if ($(this).is(':checked')) {
+    localStorage.setItem('color_mode', 'dark');
+  } else {
+    localStorage.setItem('color_mode', 'light');
+    document.body.removeAttribute('data-theme');
+  }
+  update_color_theme();
+});
+
+window.addEventListener('load', function() {
+  update_color_theme();
+});
