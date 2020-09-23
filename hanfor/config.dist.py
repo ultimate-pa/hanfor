8c0b9d866eafc1022bf397a115eb98dbc0f7bebe
Samuel <roths@informatik.uni-freiburg.de>
2020-09-23 16:53:54 +0200

    Add api url to config.

--- hanfor/config.dist.py
+++ hanfor/config.dist.py
@@ -89,6 +89,12 @@ PORT = os.environ.get('HANFOR_PORT', default=5000)
 HOST = os.environ.get('HANFOR_HOST',default='127.0.0.1')
 
 ################################################################################
+#                               Ultimate API                                   #
+################################################################################
+
+ULTIMATE_API_URL = 'http://localhost:8080'
+
+################################################################################
 #                               Miscellaneous                                  #
 ################################################################################
 # Additional static terms to be included into the autocomplete field.
