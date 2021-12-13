35672a58e44c29a88e2cb64454ad5c9c7178bfc0
hauff <hauffn@informatik.uni-freiburg.de>
2021-12-13 12:49:21 +0100

    added: pea test cases, improved: pea construction performance, fixed pea construction bugs

--- hanfor/simulator/phase_event_automaton.py
+++ hanfor/simulator/phase_event_automaton.py
@@ -1,168 +1,269 @@
 from __future__ import annotations
 
+import time
+from dataclasses import dataclass
+
 from pysmt.fnode import FNode
-from pysmt.shortcuts import TRUE, And, FALSE, Or, Not, Symbol, LT, GE
+from pysmt.shortcuts import TRUE, And, FALSE, Or, Not, Symbol, LT, GE, LE, is_valid, Iff, Solver, is_sat
 from pysmt.typing import INT
 
 from simulator.counter_trace import CounterTrace
 
-
-class Phase():
-    def __init__(self) -> None:
-        self.phase_sets: Phase.PhaseSets = Phase.PhaseSets(set(), set(), set(), set())
-
-    class PhaseSets():
-        def __init__(self, gteq: set[int] = None, wait: set[int] = None, less: set[int] = None,
-                     active: set[int] = None) -> None:
-            self.gteq: set[int] = set() if gteq is None else gteq
-            self.wait: set[int] = set() if wait is None else wait
-            self.less: set[int] = set() if less is None else less
-            self.active: set[int] = set() if active is None else active
-
-        def __eq__(self, o) -> bool:
-            return isinstance(o, Phase.PhaseSets) and \
-                   self.gteq == o.gteq and self.wait == o.wait and self.less == o.less and self.active == o.active
-
-        def __str__(self) -> str:
-            return "gteq = %s, wait = %s, less = %s, active = %s" % (
-            self.gteq if self.gteq else {}, self.wait if self.wait else {}, self.less if self.less else {},
-            self.active if self.active else {})
-
-        def add_gteq(self, i: int) -> Phase.PhaseSets:
-            return Phase.PhaseSets(self.gteq.union({i}), self.wait.union({i}), self.less.copy(), self.active.union({i}))
-
-        def add_wait(self, i: int) -> Phase.PhaseSets:
-            return Phase.PhaseSets(self.gteq.copy(), self.wait.union({i}), self.less.copy(), self.active.union({i}))
-
-        def add_less(self, i: int) -> Phase.PhaseSets:
-            return Phase.PhaseSets(self.gteq.copy(), self.wait.copy(), self.less.union({i}), self.active.union({i}))
-
-        def add_active(self, i: int) -> Phase.PhaseSets:
-            return Phase.PhaseSets(self.gteq.copy(), self.wait.copy(), self.less.copy(), self.active.union({i}))
+SOLVER_NAME = 'cvc4'
+SOLVER = Solver(name=SOLVER_NAME)
 
 
-def build_automaton(ct: CounterTrace) -> tuple[list[tuple[Phase.PhaseSets, Phase.PhaseSets, FNode, set[str]]], list[
-    tuple[Phase.PhaseSets, Phase.PhaseSets, FNode, set[str]]]]:
-    result_init, result, visited, pending = [], [], [], []
+@dataclass(frozen=True)
+class Sets:
+    gteq: frozenset[int] = frozenset()
+    wait: frozenset[int] = frozenset()
+    less: frozenset[int] = frozenset()
+    active: frozenset[int] = frozenset()
 
-    while pending or not result_init:
-        if result_init:
-            p = pending.pop()
-            visited.append(p)
+    def __str__(self) -> str:
+        return "gteq = %s, wait = %s, less = %s, active = %s" % (
+            {*self.gteq} if self.gteq else {}, {*self.wait} if self.wait else {}, {*self.less} if self.less else {},
+            {*self.active} if self.active else {})
+
+    def is_empty(self) -> bool:
+        return len(self.gteq) == 0 and len(self.wait) == 0 and len(self.less) == 0 and len(self.active) == 0
+
+    def add_gteq(self, i: int) -> Sets:
+        return Sets(self.gteq.union({i}), self.wait.union({i}), self.less.copy(), self.active.union({i}))
+
+    def add_wait(self, i: int) -> Sets:
+        return Sets(self.gteq.copy(), self.wait.union({i}), self.less.copy(), self.active.union({i}))
+
+    def add_less(self, i: int) -> Sets:
+        return Sets(self.gteq.copy(), self.wait.copy(), self.less.union({i}), self.active.union({i}))
+
+    def add_active(self, i: int) -> Sets:
+        return Sets(self.gteq.copy(), self.wait.copy(), self.less.copy(), self.active.union({i}))
+
+
+@dataclass(eq=False, frozen=True)
+class Phase:
+    state_invariant: FNode = TRUE()
+    clock_invariant: FNode = TRUE()
+    sets: Sets() = Sets()
+
+    def __eq__(self, o: Phase) -> bool:
+        return isinstance(o, Phase) and o.sets == self.sets and \
+               is_valid(Iff(o.state_invariant, self.state_invariant), solver_name=SOLVER_NAME) and \
+               is_valid(Iff(o.clock_invariant, self.clock_invariant), solver_name=SOLVER_NAME)
+
+    def __str__(self) -> str:
+        return 'state_invariant: "%s" | clock_invariant: "%s" | sets: "%s"' % (
+            self.state_invariant, self.clock_invariant, self.sets)
+
+    @staticmethod
+    def compute_state_invariant(ct: CounterTrace, p: Sets) -> FNode:
+        inactive = {*range(len(ct.dc_phases))} - p.active
+
+        result = And(*[ct.dc_phases[i].invariant for i in p.active],
+                     *[Not(ct.dc_phases[i].invariant) for i in inactive if can_seep(p, i) == TRUE()])
+
+        return result.simplify()
+
+    @staticmethod
+    def compute_clock_invariant(ct: CounterTrace, p: Sets) -> FNode:
+        result = And(LE(Symbol('c' + str(i), INT), ct.dc_phases[i].bound) for i in p.active if
+                     i in p.wait or ct.dc_phases[i].is_upper_bound() and can_seep(p, i) == FALSE())
+
+        return result.simplify()
+
+
+@dataclass(eq=False, frozen=True)
+class Transition:
+    src: Phase = None
+    dst: Phase = None
+    guard: FNode = TRUE()
+    resets: frozenset[str] = frozenset()
+
+    def __eq__(self, o: Transition) -> bool:
+        return isinstance(o, Transition) and o.src == self.src and o.dst == self.dst and o.resets == self.resets and \
+               is_valid(Iff(o.guard, self.guard), solver_name=SOLVER_NAME)
+
+    def __str__(self) -> str:
+        return 'src: "%s" | dst: "%s" | guard: "%s" | resets: "%s"' % (self.src, self.dst, self.guard, self.resets)
+
+
+class PhaseEventAutomaton:
+    def __init__(self):
+        self.init_phases: list[Phase] = []
+        self.phases: list[Phase] = []
+        self.init_transitions: list[Transition] = []
+        self.transitions: list[Transition] = []
+
+    def __eq__(self, o: PhaseEventAutomaton) -> bool:
+        return isinstance(o, PhaseEventAutomaton) and self.compare(o.phases, self.phases) and \
+               self.compare(o.transitions, self.transitions)
+
+    @staticmethod
+    def compare(l1, l2):
+        l1 = list(l1)
+        try:
+            for i in l2:
+                l1.remove(i)
+        except ValueError:
+            return False
+        return not l1
+
+    def add_phase(self, phase: Phase, init: bool = False):
+        self.init_phases.append(phase) if init else self.phases.append(phase)
+        #self.phases.append(phase)
+        #return len(self.phases) - 1
+
+    def add_transition(self, transition: Transition, init: bool) -> None:
+        self.init_transitions.append(transition) if init else self.transitions.append(transition)
+        #self.transitions.append(transition)
+
+
+def build_automaton(ct: CounterTrace) -> PhaseEventAutomaton:
+    t = time.perf_counter()
+
+    pea = PhaseEventAutomaton()
+    #indices = {}
+    visited, pending = set(), set()
+    init = True
+
+    while pending or init:
+        if init:
+            p = Sets()
+            #indices[p] = -1
         else:
-            p = Phase.PhaseSets()
+            p = pending.pop()
+            visited.add(p)
 
-        enter, keep = compute_enter_keep(ct, p if result_init else None)
-        successors = build_successors(0, p, Phase.PhaseSets(), set(), TRUE(), ct, enter, keep)
+        enter, keep = compute_enter_keep(ct, p, init)
+        successors = build_successors(0, p, Sets(), set(), TRUE(), ct, enter, keep)
 
-        pending.extend([s[1] for s in successors if s[1] not in visited and s[1] not in pending])
-        result.extend(successors) if result_init else result_init.extend(successors)
+        for s in successors:
+            src = None if init else Phase(Phase.compute_state_invariant(ct, p), Phase.compute_clock_invariant(ct, p), p)
+            dst = Phase(Phase.compute_state_invariant(ct, s[0]), Phase.compute_clock_invariant(ct, s[0]), s[0])
 
-    return result_init, result
+            if s[0] not in visited.union(pending):
+                pea.add_phase(dst, init)
+                pending.add(s[0])
+
+            pea.add_transition(Transition(src, dst, s[1], frozenset(s[2])), init)
+
+        init = False
+
+    print('TIME: %0.4f ms' % ((time.perf_counter() - t) * 1000))
+    return pea
 
 
-def build_successors(i: int, p: Phase.PhaseSets, p_: Phase.PhaseSets, reset_clocks: set[str], guard: FNode,
-                     ct: CounterTrace, enter: dict[int, FNode], keep: dict[int, FNode]) \
-        -> list[tuple[Phase.PhaseSets, Phase.PhaseSets, FNode, set[str]]]:
+cache = {}
+
+
+def build_successors(i: int, p: Sets, p_: Sets, resets: set[str], guard: FNode, ct: CounterTrace,
+                     enter: dict[int, FNode], keep: dict[int, FNode]) -> list[tuple[Sets, FNode, set[str]]]:
     result = []
+    guard = guard.simplify()
 
     # Abort if guard is not satisfiable.
-    if guard.simplify() == FALSE():
+    if guard != TRUE() and (guard == FALSE() or not is_sat(guard, solver_name=SOLVER_NAME)):
         return []
 
     # Check if successor and guard are complete.
     if i >= len(ct.dc_phases):
         # Add successor if last phase not included.
         if i - 1 not in p_.active:
-            return [(p, p_, guard.simplify(), reset_clocks)]
+            return [(p_, guard, resets)]
 
         return []
 
     # Seep depends on partial successor location.
-    seep = And(can_seep(p_, i), ct.dc_phases[i].invariant)
+    seep = And(can_seep(p_, i), ct.dc_phases[i].invariant).simplify()
     # Case 1: i not in successor.active.
-    result.extend(build_successors(i + 1, p, p_, reset_clocks,
-                                   And(guard, Not(Or(enter[i], keep[i], seep))),
+    result.extend(build_successors(i + 1, p, p_, resets,
+                                   And(guard, Not(enter[i]), Not(keep[i]), Not(seep)),
                                    ct, enter, keep))
 
     # Case 2: i in p_.active.
-    guard = And(guard, Or(enter[i], keep[i], seep))
+    guard = And(guard, Or(enter[i], keep[i], seep)).simplify()
 
     if ct.dc_phases[i].is_lower_bound():
         # Case 2a: clocks[i] in reset_clocks.
         if ct.dc_phases[i].bound_type == CounterTrace.BoundTypes.GREATEREQUAL:
-            result.extend(build_successors(i + 1, p, p_.add_gteq(i), reset_clocks.union({'c' + str(i)}),
+            result.extend(build_successors(i + 1, p, p_.add_gteq(i), resets.union({'c' + str(i)}),
                                            And(guard, Not(keep[i]), enter[i]),
                                            ct, enter, keep))
 
-            result.extend(build_successors(i + 1, p, p_.add_wait(i), reset_clocks.union({'c' + str(i)}),
+            result.extend(build_successors(i + 1, p, p_.add_wait(i), resets.union({'c' + str(i)}),
                                            And(guard, Not(keep[i]), Not(enter[i])),
                                            ct, enter, keep))
         else:
-            result.extend(build_successors(i + 1, p, p_.add_wait(i), reset_clocks.union({'c' + str(i)}),
+            result.extend(build_successors(i + 1, p, p_.add_wait(i), resets.union({'c' + str(i)}),
                                            And(guard, Not(keep[i]), ct.dc_phases[i].invariant),
                                            ct, enter, keep))
 
         # Case 2b: clocks[i] not in reset_clocks.
         if i in p.wait:
-            result.extend(build_successors(i + 1, p, p_.add_gteq(i) if i in p.gteq else p_.add_wait(i), reset_clocks,
+            result.extend(build_successors(i + 1, p, p_.add_gteq(i) if i in p.gteq else p_.add_wait(i), resets,
                                            And(guard, keep[i], LT(Symbol('c' + str(i), INT), ct.dc_phases[i].bound)),
                                            ct, enter, keep))
 
-            result.extend(build_successors(i + 1, p, p_.add_active(i), reset_clocks,
+            result.extend(build_successors(i + 1, p, p_.add_active(i), resets,
                                            And(guard, keep[i], GE(Symbol('c' + str(i), INT), ct.dc_phases[i].bound)),
                                            ct, enter, keep))
         else:
-            result.extend(build_successors(i + 1, p, p_.add_active(i), reset_clocks,
+            result.extend(build_successors(i + 1, p, p_.add_active(i), resets,
                                            And(guard, keep[i]),
                                            ct, enter, keep))
 
     elif ct.dc_phases[i].is_upper_bound() and can_seep(p_, i) == FALSE():
         # Case 2c: clocks[i] in reset_clocks.
         if ct.dc_phases[i].bound_type == CounterTrace.BoundTypes.LESS:
-            result.extend(build_successors(i + 1, p, p_.add_less(i), reset_clocks.union({'c' + str(i)}),
+            result.extend(build_successors(i + 1, p, p_.add_less(i), resets.union({'c' + str(i)}),
+                                           And(guard, enter[i] or can_seep(p, i)),
+                                           ct, enter, keep))
+            '''
+            result.extend(build_successors(i + 1, p, p_.add_less(i), resets.union({'c' + str(i)}),
                                            And(guard, enter[i]),
                                            ct, enter, keep))
 
-            result.extend(build_successors(i + 1, p, p_.add_less(i), reset_clocks.union({'c' + str(i)}),
+            result.extend(build_successors(i + 1, p, p_.add_less(i), resets.union({'c' + str(i)}),
                                            And(guard, can_seep(p, i)),
                                            ct, enter, keep))
+            '''
         else:
-            result.extend(build_successors(i + 1, p, p_.add_active(i), reset_clocks.union({'c' + str(i)}),
+            result.extend(build_successors(i + 1, p, p_.add_active(i), resets.union({'c' + str(i)}),
                                            And(guard, enter[i]),
                                            ct, enter, keep))
 
-            result.extend(build_successors(i + 1, p, p_.add_less(i), reset_clocks.union({'c' + str(i)}),
+            result.extend(build_successors(i + 1, p, p_.add_less(i), resets.union({'c' + str(i)}),
                                            And(guard, enter[i], can_seep(p, i)),
                                            ct, enter, keep))
 
-            result.extend(build_successors(i + 1, p, p_.add_less(i), reset_clocks.union({'c' + str(i)}),
+            result.extend(build_successors(i + 1, p, p_.add_less(i), resets.union({'c' + str(i)}),
                                            And(guard, Not(enter[i]), can_seep(p, i)),
                                            ct, enter, keep))
 
         # Case 2e: clocks[i] not in reset_clocks.
         if i in p.less:
-            result.extend(build_successors(i + 1, p, p_.add_less(i), reset_clocks,
+            result.extend(build_successors(i + 1, p, p_.add_less(i), resets,
                                            And(guard, Not(enter[i]), Not(can_seep(p, i))),
                                            ct, enter, keep))
         else:
-            result.extend(build_successors(i + 1, p, p_.add_active(i), reset_clocks,
+            result.extend(build_successors(i + 1, p, p_.add_active(i), resets,
                                            And(guard, Not(enter[i]), Not(can_seep(p, i))),
                                            ct, enter, keep))
     else:
         # i in p_.active.
-        result.extend(build_successors(i + 1, p, p_.add_active(i), reset_clocks, guard, ct, enter, keep))
+        result.extend(build_successors(i + 1, p, p_.add_active(i), resets, guard, ct, enter, keep))
 
     return result
 
 
-def compute_enter_keep(ct: CounterTrace, p: Phase.PhaseSets = None) -> tuple[dict[int, FNode], dict[int, FNode]]:
+def compute_enter_keep(ct: CounterTrace, p: Sets, init: bool) -> tuple[dict[int, FNode], dict[int, FNode]]:
     enter_, keep_ = {}, {}
 
-    if not p:
+    if init:
         for i in range(-1, len(ct.dc_phases)):
-            allow_empty = TRUE() if ct.dc_phases[i - 1].allow_empty else FALSE()
-            enter_[i] = TRUE() if i < 0 else And(enter_[i - 1], allow_empty, ct.dc_phases[i].invariant)
+            enter_[i] = TRUE() if i < 0 else And(enter_[i - 1], TRUE() if ct.dc_phases[i - 1].allow_empty else FALSE(),
+                                                 ct.dc_phases[i].invariant).simplify()
             keep_[i] = FALSE()
     else:
         for i in range(len(ct.dc_phases)):
@@ -172,21 +273,21 @@ def compute_enter_keep(ct: CounterTrace, p: Phase.PhaseSets = None) -> tuple[dic
     return enter_, keep_
 
 
-def enter(ct: CounterTrace, p: Phase.PhaseSets, i: int) -> FNode:
-    return And(complete(ct, p, i - 1), ct.dc_phases[i].invariant)
+def enter(ct: CounterTrace, p: Sets, i: int) -> FNode:
+    return And(complete(ct, p, i - 1), ct.dc_phases[i].invariant).simplify()
 
 
-def seep(ct: CounterTrace, p: Phase.PhaseSets, i: int) -> FNode:
-    return And(can_seep(p, i), ct.dc_phases[i].invariant)
+def seep(ct: CounterTrace, p: Sets, i: int) -> FNode:
+    return And(can_seep(p, i), ct.dc_phases[i].invariant).simplify()
 
 
-def keep(ct: CounterTrace, p: Phase.PhaseSets, i: int) -> FNode:
+def keep(ct: CounterTrace, p: Sets, i: int) -> FNode:
     return And(TRUE() if i in p.active else FALSE(), ct.dc_phases[i].invariant,
-               LT(Symbol('c' + str(i), INT), ct.dc_phases[i].bound) \
-                   if ct.dc_phases[i].is_upper_bound() and can_seep(p, i) == FALSE() else TRUE())
+               LT(Symbol('c' + str(i), INT), ct.dc_phases[i].bound)
+               if ct.dc_phases[i].is_upper_bound() and can_seep(p, i) == FALSE() else TRUE()).simplify()
 
 
-def complete(ct: CounterTrace, p: Phase.PhaseSets, i: int) -> FNode:
+def complete(ct: CounterTrace, p: Sets, i: int) -> FNode:
     result = TRUE() if i in p.active else FALSE()
 
     if i in p.wait:
@@ -197,8 +298,8 @@ def complete(ct: CounterTrace, p: Phase.PhaseSets, i: int) -> FNode:
     if i > 0 and ct.dc_phases[i].allow_empty:
         result = Or(result, complete(ct, p, i - 1))
 
-    return result
+    return result.simplify()
 
 
-def can_seep(p: Phase.PhaseSets, i: int) -> FNode:
+def can_seep(p: Sets, i: int) -> FNode:
     return TRUE() if i - 1 in p.active.difference(p.wait) else FALSE()
--- hanfor/tests/test_simulator/test_phase_event_automaton.py
+++ hanfor/tests/test_simulator/test_phase_event_automaton.py
@@ -1,21 +1,115 @@
 from unittest import TestCase
 
 from lark.lark import Lark
-from pysmt.shortcuts import Symbol
+from pysmt.shortcuts import Symbol, And, LT, GE, Or, LE, TRUE, Not, FALSE
 from pysmt.typing import INT
 
 from simulator.counter_trace import CounterTraceTransformer
-from simulator.phase_event_automaton import build_automaton
+from simulator.phase_event_automaton import build_automaton, PhaseEventAutomaton, Transition, Phase, Sets
+
+parser = Lark.open('../../simulator/counter_trace_grammar.lark', rel_to=__file__, start='counter_trace', parser='lalr')
 
 
-class TestCounterTrace(TestCase):
-    def test_bnd_response_pattern_ut(self):
+class TestPhaseEventAutomaton(TestCase):
+    def test_true(self):
+        expected = PhaseEventAutomaton()
+
+        ct_str = 'true;true'
+        expressions = {'P': Symbol('P'), 'R': Symbol('R'), 'S': Symbol('S'), 'T': Symbol('T', INT)}
+        ct = CounterTraceTransformer(expressions).transform(parser.parse(ct_str))
+        actual = build_automaton(ct)
+
+        self.assertEqual(expected, actual, msg="Error while building phase event automaton.")
+
+    def test_false(self):
+        expected = PhaseEventAutomaton()
+
+        # ct0_st
+        p1_invariant = TRUE()
+        p1 = Phase(p1_invariant, TRUE(), Sets(active=frozenset({})))
+        expected.init_phases.append(p1)
+
+        expected.init_transitions.append(Transition(None, p1, p1_invariant))
+        expected.transitions.append(Transition(p1, p1, p1_invariant))
+
+        ct_str = '⌈P⌉;true'
+        expressions = {'P': FALSE()}
+        ct = CounterTraceTransformer(expressions).transform(parser.parse(ct_str))
+        actual = build_automaton(ct)
+
+        self.assertEqual(expected, actual, msg="Error while building phase event automaton.")
+
+    def test_bnd_response_pattern_ut_globally(self):
+        expected = PhaseEventAutomaton()
+        S, R, T, c2 = Symbol('S'), Symbol('R'), Symbol('T', INT), Symbol('c2', INT)
+
+        # ct0_st0
+        p1_invariant = Or(S, Not(R))
+        p1 = Phase(p1_invariant, TRUE(), Sets(active=frozenset({0})))
+        expected.init_phases.append(p1)
+        # ct0_st012W
+        p2_invariant = And(Not(S), R)
+        p2 = Phase(p2_invariant, LE(c2, T), Sets(wait=frozenset({2}), active=frozenset({0, 1, 2})))
+        expected.init_phases.append(p2)
+        # ct0_st02W
+        p3_invariant = And(Not(S), Not(R))
+        p3 = Phase(p3_invariant, LE(c2, T), Sets(wait=frozenset({2}), active=frozenset({0, 2})))
+        expected.phases.append(p3)
+
+        # ct0_st0
+        expected.init_transitions.append(Transition(None, p1, p1_invariant))
+        expected.transitions.append(Transition(p1, p1, p1_invariant))
+        expected.transitions.append(Transition(p1, p2, p2_invariant, frozenset({'c2'})))
+        # ct0_st012W
+        expected.init_transitions.append(Transition(None, p2, p2_invariant, frozenset({'c2'})))
+        expected.transitions.append(Transition(p2, p2, And(p2_invariant, LT(c2, T))))
+        expected.transitions.append(Transition(p2, p1, And(p1_invariant, S)))
+        expected.transitions.append(Transition(p2, p3, And(p3_invariant, LT(c2, T))))
+        # ct0_st02W
+        expected.transitions.append(Transition(p3, p3, And(p3_invariant, LT(c2, T))))
+        expected.transitions.append(Transition(p3, p1, And(p1_invariant, S)))
+        expected.transitions.append(Transition(p3, p2, And(p2_invariant, LT(c2, T))))
+
         ct_str = 'true;⌈(!S && R)⌉;⌈!S⌉ ∧ ℓ > T;true'
         expressions = {'R': Symbol('R'), 'S': Symbol('S'), 'T': Symbol('T', INT)}
-
-        parser = Lark.open("../../simulator/counter_trace_grammar.lark", rel_to=__file__, start='counter_trace',
-                           parser='lalr')
         ct = CounterTraceTransformer(expressions).transform(parser.parse(ct_str))
+        actual = build_automaton(ct)
 
-        init_locations, locations = build_automaton(ct)
-        print("there we go")
+        self.assertEqual(expected, actual, msg="Error while building phase event automaton.")
+
+    def test_duration_bound_l_pattern_globally(self):
+        expected = PhaseEventAutomaton()
+        R, T, c2 = Symbol('R'), Symbol('T', INT), Symbol('c2', INT)
+
+        # ct0_st0
+        p1_invariant = R
+        p1 = Phase(p1_invariant, TRUE(), Sets(active=frozenset({0})))
+        expected.init_phases.append(p1)
+        # ct0_st01
+        p2_invariant = Not(R)
+        p2 = Phase(p2_invariant, TRUE(), Sets(active=frozenset({0, 1})))
+        expected.init_phases.append(p2)
+        # ct0_st02
+        p3_invariant = R
+        p3 = Phase(p3_invariant, LE(c2, T), Sets(less=frozenset({2}), active=frozenset({0, 2})))
+        expected.phases.append(p3)
+
+        # ct0_st0
+        expected.init_transitions.append(Transition(None, p1, p1_invariant))
+        expected.transitions.append(Transition(p1, p1, p1_invariant))
+        expected.transitions.append(Transition(p1, p2, p2_invariant))
+        # ct0_st01
+        expected.init_transitions.append(Transition(None, p2, p2_invariant))
+        expected.transitions.append(Transition(p2, p2, p2_invariant))
+        expected.transitions.append(Transition(p2, p3, p3_invariant, frozenset({'c2'})))
+        # ct0_st02
+        expected.transitions.append(Transition(p3, p3, And(p3_invariant, LT(c2, T))))
+        expected.transitions.append(Transition(p3, p1, And(p1_invariant, GE(c2, T))))
+        expected.transitions.append(Transition(p3, p2, And(p2_invariant, Or(R, GE(c2, T)))))
+
+        ct_str = 'true;⌈!R⌉;⌈R⌉ ∧ ℓ < T;⌈!R⌉;true'
+        expressions = {'R': Symbol('R'), 'T': Symbol('T', INT)}
+        ct = CounterTraceTransformer(expressions).transform(parser.parse(ct_str))
+        actual = build_automaton(ct)
+
+        self.assertEqual(expected, actual, msg="Error while building phase event automaton.")
