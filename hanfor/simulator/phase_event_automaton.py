a2ad464346a22d5112b66c01485250753327e7b3
hauff <hauffn@informatik.uni-freiburg.de>
2021-12-21 21:25:14 +0100

    fixed: primed vars in guard

--- hanfor/simulator/phase_event_automaton.py
+++ hanfor/simulator/phase_event_automaton.py
@@ -5,7 +5,7 @@ from collections import defaultdict
 from dataclasses import dataclass
 
 from pysmt.fnode import FNode
-from pysmt.shortcuts import TRUE, And, FALSE, Or, Not, Symbol, LT, GE, LE, is_valid, Iff, Solver, is_sat
+from pysmt.shortcuts import TRUE, And, FALSE, Or, Not, Symbol, LT, GE, LE, is_valid, Iff, Solver, is_sat, substitute
 from pysmt.typing import INT
 
 from simulator.counter_trace import CounterTrace
@@ -22,9 +22,22 @@ class Sets:
     active: frozenset[int] = frozenset()
 
     def __str__(self) -> str:
-        return "gteq = %s, wait = %s, less = %s, active = %s" % (
-            {*self.gteq} if self.gteq else {}, {*self.wait} if self.wait else {}, {*self.less} if self.less else {},
-            {*self.active} if self.active else {})
+        result = ''
+        for i in self.active:
+            result += str(i)
+            result += 'ᴳ' if i in self.gteq else 'ᵂ' if i in self.wait else 'ᴸ' if i in self.less else ''
+        return result
+
+        # if i in self.gteq:
+        #    result += '≥'
+        # elif i in self.wait:
+        #    result += '>'
+        # elif i in self.less:
+        #    result += '<'
+
+        # return "gteq = %s, wait = %s, less = %s, active = %s" % (
+        #    {*self.gteq} if self.gteq else {}, {*self.wait} if self.wait else {}, {*self.less} if self.less else {},
+        #    {*self.active} if self.active else {})
 
     def is_empty(self) -> bool:
         return len(self.gteq) == 0 and len(self.wait) == 0 and len(self.less) == 0 and len(self.active) == 0
@@ -57,7 +70,7 @@ class Phase:
         return hash((self.sets))
 
     def __str__(self) -> str:
-        return 'state_invariant: "%s" | clock_invariant: "%s" | sets: "%s"' % (
+        return 's_inv: "%s" | c_inv: "%s" | sets: "%s"' % (
             self.state_invariant, self.clock_invariant, self.sets)
 
     @staticmethod
@@ -92,7 +105,9 @@ class Transition:
         return hash((self.src, self.dst, self.resets))
 
     def __str__(self) -> str:
-        return 'src: "%s" | dst: "%s" | guard: "%s" | resets: "%s"' % (self.src, self.dst, self.guard, self.resets)
+        return 'src: "%s" | dst: "%s" | guard: "%s" | resets: "%s"' % (
+            None if self.src is None else self.src.sets, self.dst.sets, self.guard,
+            {*self.resets} if self.resets else '{}')
 
 
 class PhaseEventAutomaton:
@@ -159,11 +174,16 @@ def build_successors(i: int, p: Sets, p_: Sets, resets: set[str], guard: FNode, 
 
         return []
 
+    # TODO: Check if primed invariant is neccessary.
+    inv = substitute_free_variables(ct.dc_phases[i].invariant)
+
     # Seep depends on partial successor location.
-    seep = And(can_seep(p_, i), ct.dc_phases[i].invariant).simplify()
+    seep = And(can_seep(p_, i), inv).simplify()
+    # seep = And(can_seep(p_, i), ct.dc_phases[i].invariant).simplify()
+
     # Case 1: i not in successor.active.
     result.extend(build_successors(i + 1, p, p_, resets,
-                                   And(guard, Not(enter[i]), Not(keep[i]), Not(seep)),
+                                   And(guard, Not(Or(enter[i], keep[i], seep))),
                                    ct, enter, keep))
 
     # Case 2: i in p_.active.
@@ -181,8 +201,11 @@ def build_successors(i: int, p: Sets, p_: Sets, resets: set[str], guard: FNode, 
                                            ct, enter, keep))
         else:
             result.extend(build_successors(i + 1, p, p_.add_wait(i), resets.union({'c' + str(i)}),
-                                           And(guard, Not(keep[i]), ct.dc_phases[i].invariant),
+                                           And(guard, Not(keep[i])),
                                            ct, enter, keep))
+            #result.extend(build_successors(i + 1, p, p_.add_wait(i), resets.union({'c' + str(i)}),
+            #                               And(guard, Not(keep[i])),
+            #                               ct, enter, keep))
 
         # Case 2b: clocks[i] not in reset_clocks.
         if i in p.wait:
@@ -242,13 +265,23 @@ def build_successors(i: int, p: Sets, p_: Sets, resets: set[str], guard: FNode, 
     return result
 
 
+def substitute_free_variables(fnode: FNode, suffix: str = "_"):
+    symbols = fnode.get_free_variables()
+    subs = {s: Symbol(s.symbol_name() + suffix, s.symbol_type()) for s in symbols}
+    result = substitute(fnode, subs)
+    return result
+
+
 def compute_enter_keep(ct: CounterTrace, p: Sets, init: bool) -> tuple[dict[int, FNode], dict[int, FNode]]:
     enter_, keep_ = {}, {}
 
     if init:
         for i in range(-1, len(ct.dc_phases)):
+            inv = substitute_free_variables(ct.dc_phases[i].invariant)
             enter_[i] = TRUE() if i < 0 else And(enter_[i - 1], TRUE() if ct.dc_phases[i - 1].allow_empty else FALSE(),
-                                                 ct.dc_phases[i].invariant).simplify()
+                                                 inv).simplify()
+            # enter_[i] = TRUE() if i < 0 else And(enter_[i - 1], TRUE() if ct.dc_phases[i - 1].allow_empty else FALSE(),
+            #                                     ct.dc_phases[i].invariant).simplify()
             keep_[i] = FALSE()
     else:
         for i in range(len(ct.dc_phases)):
@@ -259,17 +292,26 @@ def compute_enter_keep(ct: CounterTrace, p: Sets, init: bool) -> tuple[dict[int,
 
 
 def enter(ct: CounterTrace, p: Sets, i: int) -> FNode:
-    return And(complete(ct, p, i - 1), ct.dc_phases[i].invariant).simplify()
+    inv = substitute_free_variables(ct.dc_phases[i].invariant)
+    return And(complete(ct, p, i - 1), inv).simplify()
+    # return And(complete(ct, p, i - 1), ct.dc_phases[i].invariant).simplify()
 
 
 def seep(ct: CounterTrace, p: Sets, i: int) -> FNode:
-    return And(can_seep(p, i), ct.dc_phases[i].invariant).simplify()
+    inv = substitute_free_variables(ct.dc_phases[i].invariant)
+    return And(can_seep(p, i), inv).simplify()
+    # return And(can_seep(p, i), ct.dc_phases[i].invariant).simplify()
 
 
 def keep(ct: CounterTrace, p: Sets, i: int) -> FNode:
-    return And(TRUE() if i in p.active else FALSE(), ct.dc_phases[i].invariant,
+    inv = substitute_free_variables(ct.dc_phases[i].invariant)
+    return And(TRUE() if i in p.active else FALSE(), inv,
                LT(Symbol('c' + str(i), INT), ct.dc_phases[i].bound)
                if ct.dc_phases[i].is_upper_bound() and can_seep(p, i) == FALSE() else TRUE()).simplify()
+
+    # return And(TRUE() if i in p.active else FALSE(), ct.dc_phases[i].invariant,
+    #           LT(Symbol('c' + str(i), INT), ct.dc_phases[i].bound)
+    #           if ct.dc_phases[i].is_upper_bound() and can_seep(p, i) == FALSE() else TRUE()).simplify()
 
 
 def complete(ct: CounterTrace, p: Sets, i: int) -> FNode:
--- hanfor/tests/test_simulator/test_simulator.py
+++ hanfor/tests/test_simulator/test_simulator.py
@@ -5,7 +5,6 @@ from pysmt.shortcuts import Int
 
 from simulator.counter_trace import CounterTraceTransformer
 from simulator.phase_event_automaton import build_automaton
-from simulator.simulator import Simulator
 from tests.test_simulator.test_counter_trace import testcases
 
 parser = Lark.open('../../simulator/counter_trace_grammar.lark', rel_to=__file__, start='counter_trace', parser='lalr')
@@ -17,5 +16,5 @@ class TestPhaseEventAutomaton(TestCase):
         expressions['T'] = Int(5)
         ct = CounterTraceTransformer(expressions).transform(parser.parse(ct_str))
         pea = build_automaton(ct)
-        simulator = Simulator(ct, pea)
-        simulator.simulate()
+        #simulator = Simulator(ct, pea)
+        #simulator.simulate()
