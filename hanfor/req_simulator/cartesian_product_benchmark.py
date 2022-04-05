fa3b12a832c9384cc41b3b5121ac3df57201f061
hauff <hauffn@informatik.uni-freiburg.de>
2022-04-05 15:58:54 +0200

    bugfix

--- hanfor/req_simulator/cartesian_product_benchmark.py
+++ hanfor/req_simulator/cartesian_product_benchmark.py
@@ -6,37 +6,40 @@ import time
 from collections import defaultdict
 
 from pysmt.fnode import FNode
-from pysmt.shortcuts import Symbol, EqualsOrIff, Int, is_sat, And, TRUE
+from pysmt.shortcuts import Symbol, EqualsOrIff, Int, is_sat, And, TRUE, reset_env, get_env
 from pysmt.typing import INT
 
 import boogie_parsing
 from req_simulator.boogie_pysmt_transformer import BoogiePysmtTransformer
 from termcolor import colored
 
-SOLVER_NAME = None
+SOLVER_NAME = 'z3'
+LOGIC = 'QF_LRA'
 parser = boogie_parsing.get_parser_instance()
 stats = defaultdict(dict)
 
 
 def print_stats() -> None:
     min_duration = min([stats[s]['duration'] for s in stats])
-    min_sat_duration = min([sum(stats[s]['check_sat_durations']) for s in stats])
+    min_check_sat_duration = min([stats[s]['check_sat_duration'] for s in stats])
+    min_check_sat_duration_avg = min([stats[s]['check_sat_duration_avg'] for s in stats])
 
     for s in stats:
         duration_color = 'green' if stats[s]["duration"] <= min_duration else 'white'
-        check_sat_durations_color = 'green' if sum(stats[s]['check_sat_durations']) <= min_sat_duration else 'white'
+        check_sat_duration_color = 'green' if stats[s]['check_sat_duration'] <= min_check_sat_duration else 'white'
+        check_sat_duration_avg_color = 'green' if stats[s]['check_sat_duration_avg'] <= min_check_sat_duration_avg else 'white'
 
         print(f'\n{s}:')
         print(colored(f'duration: {stats[s]["duration"]}', duration_color))
-        print('result:', stats[s]['result'])
+        #print('result:', stats[s]['result'])
         print('result size:', f'{len(stats[s]["result"])}')
         print('check sat calls:', stats[s]['check_sat_calls'])
-        print(colored(f'check sat duration: {sum(stats[s]["check_sat_durations"])}', check_sat_durations_color))
-        print('check sat duration avg:', sum(stats[s]['check_sat_durations']) / len(stats[s]['check_sat_durations']))
+        print(colored(f'check sat duration: {stats[s]["check_sat_duration"]}', check_sat_duration_color))
+        print('check sat duration avg:', stats[s]['check_sat_duration_avg'])
         # print('check sat formulas:', stats[s]['check_sat_formulas'])
 
     print('\nresults are equal:',
-          stats['naive']['result'] == stats['optimized_initial_checks']['result'] and
+          #stats['naive']['result'] == stats['optimized_initial_checks']['result'] and
           stats['naive']['result'] == stats['optimized_intermediate_checks']['result'])
 
 
@@ -60,17 +63,19 @@ def check_sat(formula: FNode) -> bool:
     caller = get_caller_name()
 
     check_sat_duration = time.time()
-    result = is_sat(formula, solver_name=SOLVER_NAME)
+    result = is_sat(formula, solver_name=SOLVER_NAME, logic=LOGIC)
     check_sat_duration = time.time() - check_sat_duration
 
     stats[caller].setdefault('check_sat_calls', 0)
     stats[caller]['check_sat_calls'] += 1
 
-    stats[caller].setdefault('check_sat_durations', [])
-    stats[caller]['check_sat_durations'].append(check_sat_duration)
+    stats[caller].setdefault('check_sat_duration', 0)
+    stats[caller]['check_sat_duration'] += check_sat_duration
 
-    stats[caller].setdefault('check_sat_formulas', [])
-    stats[caller]['check_sat_formulas'].append(formula.simplify())
+    stats[caller]['check_sat_duration_avg'] = stats[caller]['check_sat_duration'] / stats[caller]['check_sat_calls']
+
+    #stats[caller].setdefault('check_sat_formulas', [])
+    #stats[caller]['check_sat_formulas'].append(formula.simplify())
 
     return result
 
@@ -80,18 +85,33 @@ def naive(inputs: list[list[FNode]]) -> list[tuple[FNode]]:
 
     # Compute cartesian product
     results_ = list(itertools.product(*inputs))
+    print(len(results_))
 
     # Check result tuples
-    for result_ in results_:
-        formula = TRUE()
+    for i in range(len(results_) - 1, 0, -1):
+        if i % 1000 == 0:
+            print(i)
 
-        for r_ in result_:
-            formula = And(formula, r_)
+        #formula = TRUE()
+        formula = And(*results_[i])
 
-        if check_sat(formula):
-            results.append(result_)
+        #for r_ in results_[i]:
+        #    formula = And(formula, r_)
 
-    return results
+        #env = get_env()
+        #formula_manager_formulae = get_env()._formula_manager.formulae.copy()
+        #stc_memoization = get_env()._stc.memoization.copy()
+
+        if not check_sat(formula):
+            del results_[i]
+            #results_.remove(result_)
+
+        #del get_env()._formula_manager.formulae[formula._content]
+        #del get_env()._stc.memoization[formula]
+        #del formula
+        #print()
+
+    return results_
 
 
 def optimized_initial_checks(inputs: list[list[FNode]]) -> list[tuple[FNode]]:
@@ -120,10 +140,10 @@ def optimized_initial_checks(inputs: list[list[FNode]]) -> list[tuple[FNode]]:
         for r_ in result_:
             formula = And(formula, r_)
 
-        if check_sat(formula):
-            results.append(result_)
+        if not check_sat(formula):
+            results.remove(result_)
 
-    return results
+    return results_
 
 
 def optimized_intermediate_checks(inputs: list[list[FNode]]) -> list[tuple[FNode]]:
@@ -167,15 +187,33 @@ def optimized_intermediate_checks(inputs: list[list[FNode]]) -> list[tuple[FNode
 
 def main():
     inputs = [
-        ['false', 'true', 'true', 'true'],
-        ['true', 'false', 'true', 'true'],
-        ['true', 'true', 'false', 'true'],
-        ['true', 'true', 'true', 'false']
+        # ['true', 'false', 'true'],
+        # ['true', 'false', 'true'],
+
+        ['a && b','b && a','c && a'],
+        ['d && !a','e && !b','f && !c'],
+        ['g && !b','h && !a','i && !c'],
+        ['a && b', 'b && a', 'c && a'],
+        ['d && !a', 'e && !b', 'f && !c'],
+        ['g && !b', 'h && !a', 'i && !c'],
+        ['a && b', 'b && a', 'c && a'],
+        ['d && !a', 'e && !b', 'f && !c'],
+        ['g && !b', 'h && !a', 'i && !c'],
+        ['a && b', 'b && a', 'c && a'],
+        ['d && !a', 'e && !b', 'f && !c'],
+        ['g && !b', 'h && !a', 'i && !c'],
     ]
 
     variables = {
-        'a': 'int',
-        'b': 'int'
+        'a': 'bool',
+        'b': 'bool',
+        'c': 'bool',
+        'd': 'bool',
+        'e': 'bool',
+        'f': 'bool',
+        'g': 'bool',
+        'h': 'bool',
+        'i': 'bool'
     }
 
     inputs = parse_inputs(inputs, variables)
@@ -184,9 +222,9 @@ def main():
     stats['naive']['result'] = naive(inputs)
     stats['naive']['duration'] = time.time() - duration
 
-    duration = time.time()
-    stats['optimized_initial_checks']['result'] = optimized_initial_checks(inputs)
-    stats['optimized_initial_checks']['duration'] = time.time() - duration
+    #duration = time.time()
+    #stats['optimized_initial_checks']['result'] = optimized_initial_checks(inputs)
+    #stats['optimized_initial_checks']['duration'] = time.time() - duration
 
     duration = time.time()
     stats['optimized_intermediate_checks']['result'] = optimized_intermediate_checks(inputs)
