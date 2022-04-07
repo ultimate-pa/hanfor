c116d0dfed60a73fa905d290b4e9c015fc9c26cd
hauff <hauffn@informatik.uni-freiburg.de>
2022-04-07 13:46:55 +0200

    fixed: test boogie pysmt transformer

--- hanfor/tests/test_req_simulator/test_boogie_pysmt_transformer.py
+++ hanfor/tests/test_req_simulator/test_boogie_pysmt_transformer.py
@@ -4,33 +4,36 @@ from parameterized import parameterized
 
 import boogie_parsing
 from req_simulator.boogie_pysmt_transformer import BoogiePysmtTransformer
+from reqtransformer import Variable
 
 parser = boogie_parsing.get_parser_instance()
 
 
 class TestBoogiePysmtTransformer(TestCase):
+    variables = {
+        'a': Variable('a', 'bool', ''),
+        'x': Variable('x', 'int', ''),
+        'y': Variable('y', 'real', '')
+    }
+
     @parameterized.expand([
         # Bool
-        # TODO: Check this.
-        # ('a && a == a', ''),
-        ({'a': 'bool'}, '!a ==> true', '((! a) -> True)'),
+        ('!a ==> true', '((! a) -> True)'),
 
         # Int
-        ({'x': 'int'}, 'x + 1 == 2 && x - 1 == 0', '(((x + 1) = 2) & ((x - 1) = 0))'),
-        ({'x': 'int'}, 'x * 2 == 8 && x / 2 == 4', '(((x * 2) = 8) & ((x / 2) = 4))'),
-        ({'x': 'int'}, '(0 < x || x < 4) && x == 2', '(((0 < x) | (x < 4)) & (x = 2))'),
-        ({'x': 'int'}, 'x >= 0 && x <= 4 && x != 2', '((0 <= x) & ((x <= 4) & (! (x = 2))))'),
-        ({'x': 'int'}, 'x > 0 && x < 2 ==> x == 1', '(((0 < x) & (x < 2)) -> (x = 1))'),
+        ('x + 1 == 2 && x - 1 == 0', '(((x + 1) = 2) & ((x - 1) = 0))'),
+        ('x * 2 == 8 && x / 2 == 4', '(((x * 2) = 8) & ((x / 2) = 4))'),
+        ('(0 < x || x < 4) && x == 2', '(((0 < x) | (x < 4)) & (x = 2))'),
+        ('x >= 0 && x <= 4 && x != 2', '((0 <= x) & ((x <= 4) & (! (x = 2))))'),
+        ('x > 0 && x < 2 ==> x == 1', '(((0 < x) & (x < 2)) -> (x = 1))'),
 
         # Real
-        ({'y': 'real'}, 'y + 1.0 == 2.0 && y - 1.0 == 0.0', '(((y + 1.0) = 2.0) & ((y - 1.0) = 0.0))'),
-        ({'y': 'real'}, 'y * 2.0 == 8.0 && y / 2.0 == 4.0', '(((y * 2.0) = 8.0) & ((y * 1/2) = 4.0))'),
-        ({'y': 'real'}, '(0.0 < y || y < 4.0) && y == 2.0', '(((0.0 < y) | (y < 4.0)) & (y = 2.0))'),
-        ({'y': 'real'}, 'y >= 0.0 && y <= 4.0 && y != 2.0', '((0.0 <= y) & ((y <= 4.0) & (! (y = 2.0))))'),
-        ({'y': 'real'}, 'y > 0.0 && y < 2.0 ==> y == 1.0', '(((0.0 < y) & (y < 2.0)) -> (y = 1.0))')
+        ('y + 1.0 == 2.0 && y - 1.0 == 0.0', '(((y + 1.0) = 2.0) & ((y - 1.0) = 0.0))'),
+        ('y * 2.0 == 8.0 && y / 2.0 == 4.0', '(((y * 2.0) = 8.0) & ((y * 1/2) = 4.0))'),
+        ('(0.0 < y || y < 4.0) && y == 2.0', '(((0.0 < y) | (y < 4.0)) & (y = 2.0))'),
+        ('y >= 0.0 && y <= 4.0 && y != 2.0', '((0.0 <= y) & ((y <= 4.0) & (! (y = 2.0))))'),
+        ('y > 0.0 && y < 2.0 ==> y == 1.0', '(((0.0 < y) & (y < 2.0)) -> (y = 1.0))')
     ])
-    def test_int_expressions(self, variables: str, test_input: str, expected: str):
-        lark_tree = parser.parse(test_input)
-        actual = str(BoogiePysmtTransformer(variables).transform(lark_tree))
-
+    def test_int_expressions(self, test_input: str, expected: str):
+        actual = str(BoogiePysmtTransformer(self.variables).transform(parser.parse(test_input)))
         self.assertEqual(expected, actual, msg='Error while transforming boogie expression to pysmt formula.')
