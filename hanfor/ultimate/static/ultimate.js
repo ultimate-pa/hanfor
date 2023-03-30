af9257bfeba7d71fa7c5b5f0d2635598d8f3a386
Tobias Kolzer <tobias@kolzer.net>
2023-03-30 10:12:14 +0200

    added result table to analysis page, #181

--- hanfor/ultimate/static/ultimate.js
+++ hanfor/ultimate/static/ultimate.js
@@ -18,10 +18,19 @@ $(document).ready(function () {
             type: 'GET',
             url: '../api/ultimate/job/' + $('#ultimate-job-select').val(),
         }).done(function (data) {
-            $('#ultimate-job-request-id').text(data['requestId'])
-            $('#ultimate-job-request-time').text(data['request_time'])
-            $('#ultimate-job-last-update').text(data['last_update'])
-            $('#ultimate-job-request-status').text(data['status'])
+            $('#ultimate-job-request-id').text(data['requestId']);
+            $('#ultimate-job-request-time').text(data['request_time']);
+            $('#ultimate-job-last-update').text(data['last_update']);
+            $('#ultimate-job-request-status').text(data['status']);
+            let resultTable = $('#ultimate-job-result-tbl');
+            clearTable(resultTable[0]);
+            for (let i = 0; i < data['result'].length; i++) {
+                let row = resultTable[0].insertRow(-1);
+                row.insertCell(-1).innerHTML = data['result'][i]['logLvl'];
+                row.insertCell(-1).innerHTML = data['result'][i]['type'];
+                row.insertCell(-1).innerHTML = data['result'][i]['shortDesc'];
+                row.insertCell(-1).innerHTML = data['result'][i]['longDesc'];
+            }
         }).fail(function (jqXHR, textStatus, errorThrown) {
             alert(errorThrown + '\n\n' + jqXHR['responseText']);
         })
@@ -45,4 +54,11 @@ function load_all_jobs() {
     }).fail(function (jqXHR, textStatus, errorThrown) {
         alert(errorThrown + '\n\n' + jqXHR['responseText']);
     });
+}
+
+function clearTable(table) {
+  let x = table.rows.length;
+  for (let i = 1; i < x; i++) {
+    table.deleteRow(1);
+  }
 }
