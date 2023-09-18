405341501ec5b18f28e354e931e6f52700436f33
Mirko Werling <werlingm@informatik.uni-freiburg.de>
2023-09-18 09:43:44 +0200

    fist version of loading the json file

--- hanfor/state_machine/test.py
+++ hanfor/state_machine/test.py
@@ -1,3 +1,31 @@
-import sys
+"""
+Mirko Werling, University of Freiburg, Department of Computer Science
+"""
 
-PC1 = "test for PC1"
+import json
+
+# Open the JSON file for reading
+with open('stateMachine_LightSwitch.json', 'r') as stateMachine:
+    # Load the JSON data
+    data = json.load(stateMachine)
+
+    # Process each JSON object
+    for item in data:
+        id = item["ID"]
+        description = item["Description"]
+        type = item["Type"]
+        action = item["Action"]
+        stateVal = item["stateVal"]
+        variable = item["variable"]
+        print(f"ID: {id}, Description: {description}, Type: {type}, stateVal: {stateVal}")
+
+print(len(data))
+print("It is", data[0]["ID"])
+print("Test")gi
+for item in data:
+    print(item)
+    if item["ID"] == 2:
+        print(f"Key with Value 2: {item}")
+# for key, value in data.items():
+  #  if value == 2:
+   #     print(f"Key: {key}, Value: {value}")
