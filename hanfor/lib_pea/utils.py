5c8c077a58796360f0a91b6868e062fdb49d1241
Langenfeld <langenfv@informatik.uni-freiburg.de>
2023-09-21 15:55:54 +0200

    cc

--- hanfor/lib_pea/utils.py
+++ hanfor/lib_pea/utils.py
@@ -1,17 +1,10 @@
-from __future__ import annotations
-
-import json
 import math
-from typing import Any
-
-import jsbeautifier
-import yaml
 from lark import Lark
 from pysmt.fnode import FNode
 from pysmt.shortcuts import Symbol, substitute
 
-SOLVER_NAME = 'z3'
-LOGIC = 'QF_NRA'
+SOLVER_NAME = "z3"
+LOGIC = "QF_NRA"
 CT_PARSER = None
 
 
@@ -19,12 +12,19 @@ def get_countertrace_parser() -> Lark:
     global CT_PARSER
 
     if CT_PARSER is None:
-        CT_PARSER = Lark.open('countertrace_grammar.lark', rel_to=__file__, start='countertrace', parser='lalr')
+        CT_PARSER = Lark.open(
+            "countertrace_grammar.lark",
+            rel_to=__file__,
+            start="countertrace",
+            parser="lalr",
+        )
 
     return CT_PARSER
 
 
-def substitute_free_variables(fnode: FNode, suffix: str = "_", do_nothing: bool = True) -> FNode:
+def substitute_free_variables(
+    fnode: FNode, suffix: str = "_", do_nothing: bool = True
+) -> FNode:
     if do_nothing:
         return fnode
 
@@ -33,7 +33,6 @@ def substitute_free_variables(fnode: FNode, suffix: str = "_", do_nothing: bool 
     result = substitute(fnode, subs)
     return result
 
+
 def num_zeros(x: float) -> int:
     return 0 if x >= 1 else -math.floor(math.log10(x)) - 1
-
-
