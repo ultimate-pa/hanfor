428bd2ac78391d3517d4500e3ea36836748880a8
Mirko Werling <werlingm@informatik.uni-freiburg.de>
2024-02-12 17:52:37 +0100

    added small state machine in list form, parsing them into requirements, added implicite transitions and forming also into requirements

--- hanfor/sm_parser.py
+++ hanfor/sm_parser.py
@@ -32,18 +32,106 @@ with open('stateMachine_LightSwitch.json', 'r') as stateMachine:
 
 # import ...
 # import hanfor.hanfor.app as app
-import reqtransformer as req
+import reqtransformer as reqtrans
+
+def from_state_machine_to_requirement(sm):
+    """
+
+    :param sm: given state machine.
+    :return: one list of requirements for each transition in state machine.
+    """
+    print("Into the function.")
+    counter = 1
+    reqs = []
+    for transition in sm:
+        # configure basics of state machine
+        current_state = transition[0]
+        successor_state = transition[2]
+        action = transition[1]
+
+        # preparing the requirement attributes
+        id = "0" + str(counter)
+        description = "From " + str(current_state) + " to " + str(successor_state) + "."
+        type_in_csv = "requirement"
+        csv_row = {str(counter): str(counter)}
+        # pos_in_csv = counter
+
+
+        req = reqtrans.Requirement(
+        id=id,
+        description=description,
+        type_in_csv=type_in_csv,
+        csv_row=csv_row,
+        pos_in_csv=counter
+        )
+        reqs.append(req)
+        counter += 1
+
+    for element in adding_implicit_transitions(sm, counter):
+        reqs.append(element)
+    print(reqs)
+
+    return reqs
+
+def adding_implicit_transitions(sm, counter):
+    list_imp_trans = []
+
+    counter_trans = counter
+    for transition in sm:
+        # configure basics of state machine
+        current_state = transition[0]
+        successor_state = current_state
+        action = transition[1]
+
+        # preparing the requirement attributes
+        id = "0" + str(counter_trans)
+        description = "From " + str(current_state) + " to " + str(successor_state) + "."
+        type_in_csv = "requirement"
+        csv_row = {str(counter_trans): str(counter_trans)}
+        # pos_in_csv = counter
+
+
+        req = reqtrans.Requirement(
+        id=id,
+        description=description,
+        type_in_csv=type_in_csv,
+        csv_row=csv_row,
+        pos_in_csv=counter_trans
+        )
+        list_imp_trans.append(req)
+        counter_trans += 1
+
+    return list_imp_trans
+
+
+
+
+
 
 if __name__ == "__main__":
-    print("Loading state machine in Hanfor.")
-    req01 = req.Requirement(
+    print("Starting loading state machine in Hanfor.")
+    state_machine = [
+        ["Off", "turn_on", "On"],
+        ["On", "turn_off", "Off"]
+
+    ]
+    print("state machine: " + str(state_machine))
+    list_req = from_state_machine_to_requirement(state_machine)
+
+    print("list of all requirements" + str(list_req))
+
+    # check the write requirements are existing.
+    for element in list_req:
+        print(element.description)
+
+    req01 = reqtrans.Requirement(
         id="01",
         description="From OFF to ON.",
         type_in_csv="requirement",
         csv_row={"1": "1"},
         pos_in_csv=1
     )
-    req02 = req.Requirement(
+    req02 = reqtrans.Requirement(
         id="02",
         description="From ON to OFF.",
         type_in_csv="requirement",
@@ -51,10 +139,10 @@ if __name__ == "__main__":
         pos_in_csv=2
     )
 
-    formreq01 = req.Formalization(1)
+    formreq01 = reqtrans.Formalization(1)
     formreq01.scoped_pattern = "BoundedResponse"
 
-    formreq02 = req.Formalization(2)
+    formreq02 = reqtrans.Formalization(2)
     formreq02.scoped_pattern = "BoundedResponse"
 
     print(req01.formalizations.keys())
@@ -65,14 +153,18 @@ if __name__ == "__main__":
   #  print(formreq01)
   #  print(formreq02)
 
-    freeform = req.Requirement._next_free_formalization_id(req02)
+    freeform = reqtrans.Requirement._next_free_formalization_id(req02)
 
     print(freeform)
 
-    All_Req = req.RequirementCollection()
+    All_Req = reqtrans.RequirementCollection()
+
+    # Done
+    # - Create low state machine
+    # - Read states and actions, and form them into requirements
+    # - Create implicit transitions, and form them into requirements
 
     # ToDo
-    #   - Create requirements with content
     #   - Add pattern to requirement
 
     print("ENDING")
