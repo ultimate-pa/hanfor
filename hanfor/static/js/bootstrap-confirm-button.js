514f3ea4af9d617e0f6b055dd1c5697a65037d8b
Tobias Kolzer <tobias@kolzer.net>
2025-04-17 16:04:48 +0200

    refactored confirm button

--- hanfor/static/js/bootstrap-confirm-button.js
+++ hanfor/static/js/bootstrap-confirm-button.js
@@ -1,14 +1,15 @@
-jQuery.fn.bootstrapConfirmButton = function(options) {
-    $(this).on('click', options.selector, function () {
-        if ($(this).data('html') === undefined) {
-            $(this).outerWidth($(this).outerWidth()).data('html', $(this).html()).html('Do it!')
+jQuery.fn.bootstrapConfirmButton = function (options) {
+  $(this).on("click", options.selector, function () {
+    const btn = this
+    if ($(btn).data("html") === undefined) {
+      $(btn).outerWidth($(btn).outerWidth()).data("html", $(btn).html()).html("Do it!")
 
-            setTimeout(function () {
-                $(this).html($(this).data('html')).removeData('html')
-            }, 2000)
-        } else {
-            $(this).html($(this).data('html')).removeData('html')
-            options.onConfirm.call($(this))
-        }
-    })
+      setTimeout(function () {
+        $(btn).html($(btn).data("html")).removeData("html")
+      }, 2000)
+    } else {
+      $(btn).html($(btn).data("html")).removeData("html")
+      options.onConfirm.call($(btn))
+    }
+  })
 }
