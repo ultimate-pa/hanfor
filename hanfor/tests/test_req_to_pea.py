c0e4052e4dbb1f86e4c8cb154d64ee3369c7dafb
Langenfeld <langenfv@informatik.uni-freiburg.de>
2025-11-27 17:37:03 +0100

    wip: setup for new formalisation tests

---
+++ hanfor/tests/test_req_to_pea.py
@@ -0,0 +1,23 @@
+from collections import defaultdict
+from unittest import TestCase
+
+from lib_core.data import Requirement, VariableCollection, Tag
+from lib_pea.req_to_pea import get_semantics_from_requirement
+
+
+class TestAbsFunction(TestCase):
+
+    def test_invariant(self):
+        r = Requirement("req1", "the Requirement is always A", "requirement", {"a": "blabla", "b": "blablubb"}, 244)
+        i, f = r.add_empty_formalization()
+        variable_collection = VariableCollection([], [r])
+        r.update_formalization(
+            i,
+            "GLOBALLY",
+            "Invariant",
+            mapping={"R": "anIntegerVar == 42", "S": "aBooleanVar"},
+            variable_collection=variable_collection,
+            standard_tags=defaultdict(lambda: Tag("test", "color", False, "")),
+        )
+        sem = get_semantics_from_requirement(r, [r], variable_collection)
+        assert False # Todo: now here should the new tests live
