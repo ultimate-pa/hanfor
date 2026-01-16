309a16cd1e0667e0979c799d61992a3726cd5ce6
LSchaffner <luis.schaffner@email.uni-freiburg.de>
2026-01-16 22:56:02 +0100

    fix for github test (config.dist)

--- hanfor/config.dist.py
+++ hanfor/config.dist.py
@@ -105,7 +105,7 @@ AI_API_URL = "URL_TO_AI_API"
 AI_MODEL_NAMES = {
     "MODEL_NAME": "DESCRIPTION",
 }
-STANDARD_AI_MODEL = "STANDARD_AI_MODEL"
+STANDARD_AI_MODEL = "MODEL_NAME"
 
 
 ################################################################################
