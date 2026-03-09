41532852bc9ca1b39894c02cfe67c9ab2d9492e5
LSchaffner <luis.schaffner@email.uni-freiburg.de>
2026-03-09 22:37:29 +0100

    highlighted desc now in a separate Thread and using threading core

--- hanfor/lib_core/startup.py
+++ hanfor/lib_core/startup.py
@@ -28,7 +28,7 @@ from configuration.tags import STANDARD_TAGS, FUNCTIONAL_TAGS
 
 from reqtransformer import RequirementCollection
 from requirements.desc_highlighting import generate_all_highlighted_desc
-from thread_handling.threading_core import ThreadHandler
+from thread_handling.threading_core import ThreadHandler, ThreadTask, SchedulingClass, ThreadGroup
 
 if config.FEATURE_AI:
     from ai_request.ai_core_requests import AiRequest
@@ -249,12 +249,21 @@ def startup_hanfor(flask_app: HanforFlask, args, here, *, no_data_tracing: bool 
     varcollection_consistency_check(flask_app, args)
 
     if config.FEATURE_VARIABLE_DESCRIPTION_HIGHLIGHTING:
-        generate_all_highlighted_desc(
-            VariableCollection(
-                flask_app.db.get_objects(Variable).values(),
-                flask_app.db.get_objects(Requirement).values(),
-            ).get_available_var_names_list(used_only=False),
-            flask_app.db.get_objects(Requirement),
+        flask_app.thread_handler.submit(
+            ThreadTask(
+                generate_all_highlighted_desc,
+                SchedulingClass.SYSTEM_CALL,
+                ThreadGroup.VARIABLE_HIGHLIGHTING,
+                None,
+                (
+                    VariableCollection(
+                        flask_app.db.get_objects(Variable).values(),
+                        flask_app.db.get_objects(Requirement).values(),
+                    ).get_available_var_names_list(used_only=False),
+                    flask_app.db.get_objects(Requirement),
+                ),
+                {},
+            )
         )
 
     return True
