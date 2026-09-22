f4460f6987f8d608d83ff0d74ab58aa3aa5b30d5
Dejan Dudukovic <dekidudukovic@gmail.com>
2026-09-22 11:40:04 +0200

    test: test requirements table behaviour, also fix #250

--- hanfor/tests/test_frontend/test_example.py
+++
@@ -1,6 +0,0 @@
-from playwright.sync_api import Page, expect
-
-
-def test_requirements_table_lists_requirements(page: Page):
-    page.goto("/")
-    expect(page.get_by_text("SysRS FooXY_42")).to_be_visible()
